54d43feb68768f356d3a79119455e6211f90a043
11Some1 <68611574+11Some1@users.noreply.github.com>
2021-08-05 20:05:29 -0400

    initial

---
+++ index.js
@@ -0,0 +1,302 @@
+'use strict';
+
+//hashmap clientsf
+const clients = {};
+const games = {};
+
+const express = require('express');
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 8000;
+const INDEX = '/index.html';
+const path = require('path');
+
+const server = express()
+    .use('/', express.static(path.join(__dirname, 'public')))
+    .get("/", (req,res)=> res.sendFile(__dirname + "/public/index.html"))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+/*
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .use(express.static('public'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}));
+*/
+const io = socketIO(server);
+
+const crawler = require('crawler-request');
+
+io.on('connection', client => {
+    console.log('Client connected');
+
+    client.on("disconnect", () => {
+        if(games[client.id] !== undefined) {
+            delete games[client.id];
+        }
+    });
+
+    client.on("download", (result) => {
+        const clientId = result.clientId;
+
+        crawl(result.packetURL).then(res => {       
+            
+            const payLoad = {
+                "success": true,
+                "containsTossups": !(res.tossups.length === 0),
+                "containsBonuses": !(res.bonuses.length === 0)
+            }    
+            
+            games[clientId] = {
+                "id": clientId,
+                "packet": res,
+                "rate": 1,
+                "curQuestion": null,
+                "score": 0,
+            }
+
+            io.to(clientId).emit("download", payLoad);
+        });
+    });
+
+    client.on("reset", (clientId) => {
+        games[clientId] = null;
+        io.to(clientId).emit("reset");
+    });
+
+    client.on("beginQuestion", (result) => {
+        const curPacket = games[result.clientId];
+        games[result.clientId].curQuestion = (result.typeQuestion === "tossup" ? 
+            curPacket.packet.tossups.shift() : curPacket.packet.bonuses.shift());
+
+        const payLoad = {
+            "question": games[result.clientId].curQuestion.question,
+            "typeQuestion": "tossup"
+        }
+
+        io.to(result.clientId).emit("beginQuestion", payLoad);
+    });
+
+    client.on("buzz", (result) => {
+        const curPacket = games[result.clientId];
+
+        const payLoad = {
+            "answer": games[result.clientId].curQuestion.answer,
+            "isTossupsEmpty": curPacket.packet.tossups.length === 0, 
+            "isBonusesEmpty": curPacket.packet.bonuses.length === 0
+        }
+
+        io.to(result.clientId).emit("buzz", payLoad);
+    });
+
+    client.on("updateScore", (result) => {
+        games[result.clientId].score += result.amnt;
+
+        const payLoad = {
+            "newScore": games[result.clientId].score
+        }
+
+        io.to(result.clientId).emit("updateScore", payLoad);
+    });
+    
+});
+
+/**
+* Crawls an online PDF of Quiz Bowl questions, parsing out the toss-up and bonuses contents (if able.)
+* @async
+* @param {String} url - The web url of a packet of questions to be parsed.
+* @returns {{tossups: String[], bonuses: String[]}} The parsed conents of the requested packet.
+*/
+async function crawl(url) {
+    
+    return new Promise(function(resolve, reject) {
+        crawler(url).then(function(response) {
+            const packetInfo = {
+                "tossups": [],
+                "bonuses": []
+            }
+
+            var str = response.text.replace(/(\r\n|\n|\r)/gm, "");
+            
+            console.log(str); // temp
+            // console.log(response.text); // temp
+    
+            var values1 = str.split(/\s+/);
+            var values2 = str.split(/\s+/);
+    
+            while(values1.length > 0) {
+                var s = values1.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+    
+                var esc = false;
+                var bypass = !str.toLowerCase().includes("tossups");
+                
+                try {
+                    if(s != undefined && (s.toLowerCase().includes("tossups") || bypass)) {
+                        if(s !== "Tossups" && s !== "TOSSUPS") {
+                            values1.unshift("1.");
+                        }
+
+                        var i = 1;
+                        while(values1.length > 0 && i <= 20) {
+                            if(i == 1) {
+                                s = values1.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                            }
+                            if(s.includes(i + ".") || s.includes("(" + i + ")")) {
+                                var question = "";
+                                var answer = "";
+                                question += (i + ". ");
+                                
+                                if(s !== ((i).toString() + ".")) {
+                                    s = s.substring(s.indexOf((i).toString() + ".") + 2);
+                                }
+                                else {
+                                    s = values1.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                                }
+                                while(!(s.toLowerCase().includes("answer"))) {
+                                    question += (s + " ");
+                                    s = values1.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                                }
+
+                                if(s !== "ANSWER:" && s !== "Answer:" && s !== "Answer: " && s !== "Answer: ") {
+                                    s = s.substring(s.indexOf(":") + 1);
+                                }
+                                else {
+                                    s = values1.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                                }
+        
+                                while(!s.includes((i+1).toString() + ".") && !s.includes("(" + (i+1).toString() + ")") && !s.toLowerCase().includes("bonuses")) {
+                                    answer += (s + " ");
+                                    s = values1.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                                    if(s.toLowerCase().includes("extra:") || s.toLowerCase().includes("tiebreaker")) {
+                                        esc = true;
+                                        break;
+                                    }
+                                }  
+                                if((i+1).toString() + "." !== s) {
+                                    values1.unshift(s.replace((i+1).toString() + ".", ""));
+                                    s = (i+1).toString() + "."
+                                }
+                                packetInfo.tossups.push({
+                                    "question": removeCharacters(question),
+                                    "answer": removeCharacters(answer)
+                                });
+                                i++;
+                                if(esc) {
+                                    break;
+                                }
+                            }
+                        }
+                    
+                    }
+                }
+                catch(TypeError) {
+    
+                }
+                if(esc) {
+                    break;
+                }
+            }
+    
+            while(values2.length > 0) {
+                var s = values2.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+    
+                var esc = false;
+                try {
+                    if(s != undefined && (s.toLowerCase().includes("bonuses"))) {
+                        if(s !== "Bonuses" && s !== "BONUSES") {
+                            values2.unshift("1.");
+                        }
+                        
+                        var i = 1;
+                        while(values2.length > 0) {
+                            if(i == 1) {
+                                s = values2.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                            }
+                            if(s.includes(i + ".") || s.includes("(" + i + ")")) {
+                                for(var j = 0; j < 3; j++) {
+                                    var question = "";
+                                    var answer = "";
+                                    if(j === 0) {
+                                        question += (i + ". ");
+                                    }
+                                    s= values2.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                                    while(!s.toString().toLowerCase().includes("answer:")) {
+                                        question += (s + " ");
+                                        s = values2.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                                    }
+
+                                    if(s !== "ANSWER:" && s !== "Answer:" && s !== "Answer: " && s !== "Answer: ") {
+                                        var idx = s.indexOf(":")
+                                        s = s.substring(idx + 1);
+                                    }
+                                    else {
+                                        s = values2.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                                    }
+                                    while(!s.includes("[10]") && !s.includes((i+1).toString() + ".")) {
+                                        answer += (s + " ");
+                                        s = values2.shift().replace(/[\u200B-\u200D\uFEFF]/g, '');
+                                        if(s.toLowerCase().includes("extra") || s.toLowerCase().includes("tiebreaker") || values2.length === 0) {
+                                            esc = true;
+                                            break;
+                                        }
+                                    }
+
+                                    packetInfo.bonuses.push({
+                                        "question": removeCharacters(question),
+                                        "answer": removeCharacters(answer)
+                                    });
+                                }
+                                i++;
+                                // packetInfo.bonuses.push(temp);
+                            }
+                            else {
+                                esc = true;
+                            }
+                            if(esc) {
+                                break;
+                            }
+                        }
+                    }
+                    if(esc) {
+                        break;
+                    }
+                }
+                catch(TypeError) {
+    
+                }
+            }
+
+            console.log(packetInfo); // temp
+            resolve(packetInfo);
+        });
+    });
+}
+
+/**
+* Removes unnecessary contents from the contents of a packet question / answer.
+* Unnecessary content is: text enclosed by brackets, parentheses, and angle brackets, 
+* as well as astericks.
+* Other minor text adjustments are also done, if needed. 
+* @param {String} str - The string representing a question or answer from a packet.
+* @returns {String} A string representing a question or answer from a packet, with all unnecessary content removed.
+*/
+function removeCharacters(str) {
+    while(str.includes("[")) {
+        str = str.substring(0, str.indexOf("[") - 1) + str.substring(str.indexOf("]") + 1);
+    }
+    while(str.includes("(")) {
+        str = str.substring(0, str.indexOf("(") - 1) + str.substring(str.indexOf(")") + 1);
+    }
+    while(str.includes("<")) {
+        str = str.substring(0, str.indexOf("<") - 1) + str.substring(str.indexOf(">") + 1);
+    }
+    if(str.includes("*")) {
+        str.replace("*", "");
+    }
+    if(str.charAt(str.length - 1) === " ") {
+        str = str.substring(0, str.length - 1);
+    }
+    if((str.match(/1./g) || []).length > 1) {
+        str = str.substring(3);
+        str = str.substring(str.indexOf("1."));
+    }
+
+    return str;
+}
---
+++ public/script.js
@@ -0,0 +1,291 @@
+let socket = io();
+let clientId = null;
+let curQuestionType = null;
+
+var synth = window.speechSynthesis;
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var rate = document.querySelector("#rate");
+var rateValue = document.querySelector(".rate-value");
+
+const introScreen = document.getElementById("introScreen");
+const functionScreen = document.getElementById("functionScreen");
+const settingsScreen = document.getElementById("settingsScreen");
+const resultScreen = document.getElementById("resultScreen");
+
+const btnExternal = document.getElementById("btnExternal");
+const btnDownload = document.getElementById("btnDownload");
+const btnReset = document.getElementById("btnReset");
+const btnTossup = document.getElementById("btnTossup");
+const btnBonus = document.getElementById("btnBonus");
+const btnBuzz = document.getElementById("btnBuzz");
+
+const txtPacketURL = document.getElementById("txtPacketURL");
+const inputNextQuestion = document.getElementById("inputNextQuestion");
+const txtScore = document.getElementById("txtScore");
+const txtAnswerResult = document.getElementById("answerResult"); // resultPara
+const txtAnswerCorrect = document.getElementById("answerCorrect"); //phrasePara
+const txtAnswerRecieved = document.getElementById("answerRecieved"); // diagnosticPara
+const txtAnswerSimilarity = document.getElementById("answerSimilarity");
+
+const PERCENTAGE_THRESHOLD = 0.83;
+
+socket.on("connect", () => {
+    clientId = socket.id;
+    clientName = clientId;
+
+    console.log(clientId);
+});
+
+btnExternal.addEventListener("click", e => {
+    window.open("https://quizbowlpackets.com/");
+});
+
+btnDownload.addEventListener("click", e => {
+
+    console.log(txtPacketURL.value)
+    if(txtPacketURL.value === "") {
+        document.getElementById("txtDownloadError").innerHTML = "Failed to download" +
+        " the packet. You did not enter a valid URL in."
+    }
+    else {
+        document.getElementById("txtDownloadError").innerHTML = "";
+        const payLoad = {
+            "clientId": clientId,
+            "packetURL": txtPacketURL.value
+        }
+
+        socket.emit("download", payLoad);
+    }
+});
+
+btnReset.addEventListener("click", e => {
+    socket.emit("reset", clientId);
+});
+
+btnTossup.addEventListener("click", e => {
+    const payLoad = {
+        "clientId": clientId,
+        "typeQuestion": "tossup"
+    }
+
+    socket.emit("beginQuestion", payLoad);
+});
+
+btnBonus.addEventListener("click", e => {
+    const payLoad = {
+        "clientId": clientId,
+        "typeQuestion": "bonus"
+    }
+
+    socket.emit("beginQuestion", payLoad);
+});
+
+btnBuzz.addEventListener("click", e => {
+    const payLoad = {
+        "clientId": clientId,
+        "typeQuestion": curQuestionType
+    }
+    socket.emit("buzz", payLoad);
+});
+
+socket.on("download", (response) => {
+    if(response.success) {
+        introScreen.style.display = "none";
+        functionScreen.style.display = "block";
+        if(response.containsTossups) {
+            btnTossup.disabled = false;
+            document.getElementById("txtNoMoreTossup").style.display = "none";
+        }
+        else {
+            document.getElementById("txtNoMoreTossup").style.display = "block";
+            btnTossup.disabled = true;
+        }
+        if(response.containsBonuses) {
+            document.getElementById("txtNoMoreBonus").style.display = "none";
+            btnBonus.disabled = false;
+        }
+        else {
+            document.getElementById("txtNoMoreBonus").style.display = "block";
+            btnBonus.disabled = true;
+        }
+        
+        updateScore(0, clientId);
+    }
+});
+
+socket.on("reset", () => {
+    introScreen.style.display = "block";
+    functionScreen.style.display = "none";
+    resultScreen.style.display = "none";
+});
+
+socket.on("beginQuestion", (response) => {
+    settingsScreen.style.display = "none";
+    btnReset.style.display = "none";
+    btnTossup.style.display = "none";
+    btnBonus.style.display = "none";
+    btnBuzz.style.display = "block";
+    curQuestionType = response.typeQuestion;
+    speak(response.question);
+});
+
+socket.on("buzz", (response) => {
+    const isNeg = synth.speaking;
+    synth.pause();
+    synth.cancel();
+    if(response.isTossupsEmpty) {
+        document.getElementById("txtNoMoreTossup").style.display = "block";
+        btnTossup.disabled = true;
+    }
+    if(response.isBonusesEmpty) {
+        document.getElementById("txtNoMoreBonus").style.display = "block";
+        btnBonus.disabled = true;
+    }
+    testSpeech(response.answer, isNeg);
+});
+
+socket.on("updateScore", (response) => {
+    txtScore.textContent = "Your Score: " + response.newScore;
+});
+
+rate.onchange = function() {
+    rateValue.textContent = rate.value;
+}
+
+function speak(text) {
+    if(synth.speaking) {
+        console.error("speechSynthesis.speaking");
+        return;
+    }
+    var utterThis = new SpeechSynthesisUtterance(text);
+    utterThis.onend = function (event) {
+        console.log("SpeechSynthesisUtterance.onend");
+    }
+    utterThis.onerror = function (event) {
+        console.error("SpeechSynthesisUtterance.onerror");
+    }
+    utterThis.rate = rate.value;
+    // utterThis.voice = "Microsoft Mark - English (United States)";
+    synth.speak(utterThis);
+}
+
+function testSpeech(text, isNeg) {
+    text = text.toLowerCase();
+
+    var grammar = "#JSGF V1.0; grammar phrase; public <phrase> = " + text +";";
+    var recognition = new SpeechRecognition();
+    var speechRecognitionList = new SpeechGrammarList();
+    speechRecognitionList.addFromString(grammar, 1);
+    recognition.grammars = speechRecognitionList;
+    recognition.lang = "en-US";
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    recognition.start();
+             
+    recognition.onresult = function(event) {
+        var speechResult = event.results[0][0].transcript.toLowerCase();
+        console.log("Confidence: " + event.results[0][0].confidence);
+                
+        txtAnswerRecieved.textContent = "Speech received: " + speechResult;
+        const percentSimilar = similarity(text.toLowerCase(), speechResult.toLowerCase());
+        txtAnswerSimilarity.textContent = "Similarity ratio: " + percentSimilar;
+        
+        if(percentSimilar > PERCENTAGE_THRESHOLD) {
+            txtAnswerResult.textContent = "Correct answer! SCORE +10";
+            txtAnswerResult.style.background = "lime";
+            updateScore(10, clientId);
+        }
+        else {
+            txtAnswerResult.style.background = "red";
+            if(isNeg) {
+                txtAnswerResult.textContent = "Incorrect answer. SCORE -5 (NEG)";
+                updateScore(-5, clientId);
+            }
+            else {
+                txtAnswerResult.textContent = "Incorrect answer.";
+            }
+        }
+        txtAnswerCorrect.textContent = "The correct answer was: " + text;
+
+        settingsScreen.style.display = "block";
+        resultScreen.style.display = "block";
+        btnReset.style.display = "block";
+        btnTossup.style.display = "block";
+        btnBonus.style.display = "block";
+        btnBuzz.style.display = "none";
+    }
+
+    recognition.onspeechend = function() {
+        recognition.stop();
+    }
+    
+    recognition.onerror = function(event) {
+        txtAnswerRecieved.textContent = "Error occurred in recognition: " + event.error;
+    }
+}
+
+function updateScore(amnt, clientId) {
+    const payLoad = {
+        "clientId": clientId,
+        "amnt": amnt
+    }
+    socket.emit("updateScore", payLoad);
+}
+
+/**
+* Determines the percent equivalency of two inputted strings. 
+* This function uses the concept of Levenshtein distance to do so (@see editDistance)
+* @param {String} s1 - The first string in question.
+* @param {String} s2 - The second string in question.
+* @returns {number} The percent equivalency of the two inputted strings.
+*/
+function similarity(s1, s2) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+        longer = s2;
+        shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+        return 1.0;
+    }
+    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+/**
+* Determines the Levenshtein distance between two inputted strings. 
+* @param {String} s1 - The first string in question.
+* @param {String} s2 - The second string in question.
+* @returns {number} The Levenshtein distance between the two inputted strings.
+*/
+function editDistance(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+  
+    var costs = new Array();
+    for (var i = 0; i <= s1.length; i++) {
+        var lastValue = i;
+        for (var j = 0; j <= s2.length; j++) {
+            if (i == 0)
+                costs[j] = j;
+            else {
+                if (j > 0) {
+                    var newValue = costs[j - 1];
+                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                    newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                    costs[j - 1] = lastValue;
+                    lastValue = newValue;
+                }
+            }
+        }
+        if (i > 0)
+            costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
+}
